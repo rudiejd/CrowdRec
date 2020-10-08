374fa640fead8e7296a946aa282ba626cf782b92
rudiejd <rudiejd@users.noreply.github.com>
2020-10-07 20:12:28 -0400

    Updates

---
+++ precache-manifest.d712f3f746b74cb39cb3b16e2340e676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c6a69d7567031ac3ed0ca404c82efe",
+    "url": "/CrowdRec/index.html"
+  },
+  {
+    "revision": "99bf1e41bac6f0b662f8",
+    "url": "/CrowdRec/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ea47bf715df1b3b5123",
+    "url": "/CrowdRec/static/js/2.c40ea8a7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CrowdRec/static/js/2.c40ea8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99bf1e41bac6f0b662f8",
+    "url": "/CrowdRec/static/js/main.2c1adbfa.chunk.js"
+  },
+  {
+    "revision": "f6094bef42744c747e1c",
+    "url": "/CrowdRec/static/js/runtime-main.d91b097d.js"
+  }
+]);
